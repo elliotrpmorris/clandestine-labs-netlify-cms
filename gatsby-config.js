2d3cb8a9d4365ca9e8c5ef475032a37e65e02ef8
Elliot <hello@elliotmorris.dev>
2020-07-06 19:12:36 +0100

    font awesome gatsby plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,6 +31,7 @@ module.exports = {
     },
     'gatsby-plugin-sharp',
     'gatsby-transformer-sharp',
+    'gatsby-plugin-fontawesome-css',
     {
       resolve: 'gatsby-transformer-remark',
       options: {
